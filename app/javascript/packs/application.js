aa3d5f202f8dd7cb1e2fd5157726d8a59b15ccb4
Margarida Toureiro <margaridatoureiro@gmail.com>
2020-06-04 23:22:19 +0100

    I can finally have some javascript and the cards are now dynamic aka they flip on click and not on hover

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+console.log("Hello from application!!");
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 import 'bootstrap';
+import { flipOnClick } from '../components/flip';
+
+document.addEventListener('turbolinks:load', () => {
+  flipOnClick();
+});
